710cafffafcdb9bc738f6ff8cc9c8875d50634ab
Sana parvin .M <kamarulaila72@gmail.com>
2026-09-19 15:39:02 +0530

    Allow Vercel frontend in CORS

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://book-api-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
