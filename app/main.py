4f3491bcd8a5c5a205331437206d909d797840e4
Sana parvin .M <kamarulaila72@gmail.com>
2026-09-19 15:42:31 +0530

    Fix CORS for Vercel frontend

--- app/main.py
+++ app/main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
-from app.routers import books
 from fastapi.middleware.cors import CORSMiddleware
-
+from app.routers import books
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "https://book-api-frontend.vercel.app"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://book-api-frontend.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 app.include_router(books.router)
+
 
 @app.get("/")
 async def read_root():
