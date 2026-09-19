bbc86302bd13004be70eedc36327543975778e3a
Sana parvin .M <kamarulaila72@gmail.com>
2026-09-19 14:17:16 +0530

    Add CORS configuration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.routers import books
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(books.router)
 
